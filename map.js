33c70ffb78e9f199e2e1975efd948afc43f1879d
Evgeniy Gordienko <112853162+eudk@users.noreply.github.com>
2023-11-27 13:31:50 +0100

    map oprettet og marker men ingen metoder

---
+++ map.js
@@ -0,0 +1,9 @@
+                    
+const map = L.map('map').setView([56.2639, 9.5018], 7);
+
+const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+L.marker([56.2639, 9.5018]).addTo(map); // add marker in the map
+
