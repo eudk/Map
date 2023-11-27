ece48ad4c59c6a58be355726cbae15e9bd3c8f41
Evgeniy Gordienko <112853162+eudk@users.noreply.github.com>
2023-11-27 14:44:02 +0100

    knapper testet ikke bootstrap

--- map.js
+++ map.js
@@ -7,3 +7,15 @@ const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 L.marker([56.2639, 9.5018]).addTo(map); // add marker in the map
 
+Vue.createApp({
+    data() {
+        
+    },
+    methods: {
+        zoomCurrentLocation() {
+            map.setView([56.2639, 9.5018], 13); // Fixed the method to use 'map' instead of 'L.map'
+        }
+    }
+    
+}).mount('#app')
+
