4ff37aac99775156bfd92b2792b1a2456dce2368
Evgeniy Gordienko <112853162+eudk@users.noreply.github.com>
2023-11-28 12:02:31 +0100

    map i observation

--- observation/observation.js
+++ observation/observation.js
@@ -2,6 +2,8 @@ const app = Vue.createApp({
     data() {
         return {
             buttonText: 'Indsend',
+            map: null,
+
         };
     },
     methods: {
@@ -56,7 +58,28 @@ const app = Vue.createApp({
             alert('Observation oprettet!');
             window.location.href = '../map.html';
             return false;
+        },
+
+        // Metode til at vise kortet
+        initializeMap() {
+            this.map = L.map('map').setView([56.2639, 9.5018], 7); // start position bare kort over dk
+            const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(this.map);
+
+            //  markering af observationer
+            const tempMarker = L.marker([56.2639, 9.5018]).addTo(this.map);
+            tempMarker.bindPopup('Observation').openPopup();
+
+            const tempMarker2 = L.marker([56.6649, 9.6418]).addTo(this.map);
+            tempMarker2.bindPopup('Observation').openPopup();
+
+            
         }
+
+
+
     },
 });
 
