239b15b4ba9b90e2c180a325b98eac2f26b3b6bd
Anders Fibiger Jensen <ande153d@edu.zealand.dk>
2023-12-01 12:45:19 +0100

    User

--- list/list.js
+++ list/list.js
@@ -1,13 +1,34 @@
+const baseurl="https://naturdanmark-api20231124193012.azurewebsites.net"
+const baseurlsearch="https://naturdanmark-api20231124193012.azurewebsites.net/Api/Observation?fromToday=true&sortMethod=datedesc"
+
 const app = Vue.createApp({
     data() {
         return {
-            searchString: null
+            searchString: null,
+            observations:[]
         };
     },
-
+    async created()
+    {
+        this.CreatedToday(baseurlsearch)
+    },
     methods: {
         submit(){
 
+        },
+        async CreatedToday(url)
+        {
+            try
+            {
+                const response= await axios.get(baseurlsearch)
+                this.observations=response.data
+                console.log("data created")
+            }
+            catch(ex)
+            {
+                alert(ex.message)
+            }
+            
         }
     },
     mounted() {
